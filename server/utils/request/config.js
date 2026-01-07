0140bf7691d80f9f1679991c93a0dccf8ae8862f
pureStyles <siyuan.wen@outlook.com>
2026-01-07 22:57:36 +0800

    feat: cookie获取方式

--- server/utils/request/config.js
+++ server/utils/request/config.js
@@ -5,24 +5,27 @@ const serverConfig = {
     useTokenAuthorization: false,
 };
 
-const getHeaders = () => ({
-    "accept": "*/*",
-    "accept-language": "zh-CN,zh;q=0.9,ar;q=0.8",
-    "cache-control": "no-cache",
-    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "pragma": "no-cache",
-    "priority": "u=1, i",
-    "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "x-requested-with": "XMLHttpRequest",
-    "cookie": process.env.USER_COOKIE,
-    "Referer": "https://www.jiaoyikecha.com/",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
-  });
+const getHeaders = () => {
+    console.log("xxx", process.env.USER_COOKIE);
+    return {
+        "accept": "*/*",
+        "accept-language": "zh-CN,zh;q=0.9,ar;q=0.8",
+        "cache-control": "no-cache",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "pragma": "no-cache",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "cookie": process.env.USER_COOKIE,
+        "Referer": "https://www.jiaoyikecha.com/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      }
+}
 
 
 module.exports = {
